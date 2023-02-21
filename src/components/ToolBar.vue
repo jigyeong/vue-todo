259ec6fd650eb39deb53a0bd5fa30e6b5fbb3e02
jigyeong <wlrud0126@gmail.com>
2023-02-21 18:11:41 +0900

    Style : Decorate Toolbar

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header">
-    <router-link to="/news">News</router-link> |
-    <router-link to="/ask">Ask</router-link> |
+    <router-link to="/news">News</router-link>
+    <router-link to="/ask">Ask</router-link>
     <router-link to="/jobs">Jobs</router-link>
   </div>
 </template>
@@ -14,16 +14,20 @@ export default {
 
 <style scoped>
     .header {
-        color: black;
-        background-color: rgb(50, 126, 123);
-        display: flex;
-        padding: 8px;
+        color: rgb(38, 38, 38);
+        background-color: white;
+        display: block;
+        padding: 10px;
+        align-items: center;
+        box-shadow: 0 1px 2px hsl(0deg 0% 0% / 20%);
     }
     .header .router-link-exact-active{
-        color:white;
+        color:rgb(23, 61, 92);
         font-weight: bold;
     }
     .header a {
-        color: black;
+        color: rgb(38, 38, 38);
+        text-decoration-line: none;
+        padding-right: 20px;
     }
 </style>
