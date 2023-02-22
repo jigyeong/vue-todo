24fe8f7dd348cf43fb8390b69f224f6a8b813dce
jigyeong <wlrud0126@gmail.com>
2023-02-22 18:01:51 +0900

    Add : Vuex

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    {{ this.$store.state.news }}
     <div v-for="user in users" :key="user.key">
         {{ user.title }}
     </div>
@@ -19,6 +20,7 @@ export default {
         console.log('created')
         fetchNewsList()
         .then(res => {
+            this.$store.state.news = res.data;
             this.users = res.data
         })
         .catch(err => console.log(err))
